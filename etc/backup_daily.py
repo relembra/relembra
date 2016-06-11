aed98545d9afbece964672de46525e25f7b671fb
Estevo U. Caamanho Castro <euccastro@gmail.com>
2016-06-11 07:02:17 +0200

    make backup script try and upload, warn on errors

--- etc/backup_daily.py
+++ etc/backup_daily.py
@@ -2,13 +2,27 @@
 
 from datetime import datetime
 import os.path
+import sys
 
 
 def weekday_abbrev():
-    return datetime.utcnow().strftime('%a').lower()
+    return datetime.now().strftime('%a').lower()
+
+def warn(txt):
+    print >> sys.stderr, "WARNING: %s!" % txt
+    os.system("""echo "At %s: %s" | mail -s "WARNING: %s" euccastro@gmail.com""" % (datetime.now(), txt, txt))
 
 def backup():
-    os.system("/opt/datomic-free-0.9.5372/bin/datomic -Xmx800m -Xms800m backup-db datomic:free://localhost:4334/relembra file:/opt/datomic-free-0.9.5372/bak/%s" % weekday_abbrev())
+    wda = weekday_abbrev()
+    backup_path = "/opt/datomic-free-0.9.5372/bak/%s" % wda
+    retcode = os.system("/opt/datomic-free-0.9.5372/bin/datomic -Xmx800m -Xms800m backup-db datomic:free://localhost:4334/relembra file:%s"
+                        % backup_path)
+    if retcode != 0:
+        warn("Couldn't backup")
+        return
+    retcode = os.system("/usr/bin/rclone --checksum %s acd:relembra-backup-%s" % wda)
+    if retcode != 0:
+        warn("Couldn't upload backup")
 
 
 if __name__ == '__main__':
