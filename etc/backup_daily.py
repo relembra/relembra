6bc945071c199a8403ddfb5877f6f61beb40cfba
Estevo U. Caamanho Castro <euccastro@gmail.com>
2016-06-11 07:08:23 +0200

    move to the bak/ directory so hopefully datomic can save backup logs

--- etc/backup_daily.py
+++ etc/backup_daily.py
@@ -14,7 +14,9 @@ def warn(txt):
 
 def backup():
     wda = weekday_abbrev()
-    backup_path = "/opt/datomic-free-0.9.5372/bak/%s" % wda
+    backup_root = "/opt/datomic-free-0.9.5372/bak"
+    backup_path = os.path.join(backup_root, wda)
+    os.chdir(backup_root)
     retcode = os.system("/opt/datomic-free-0.9.5372/bin/datomic -Xmx800m -Xms800m backup-db datomic:free://localhost:4334/relembra file:%s"
                         % backup_path)
     if retcode != 0:
