f537eda51b9032f10bda64a5f4c24b8247b3c7b6
Estevo U. Caamaño Castro <euccastro@gmail.com>
2019-11-05 03:32:07 +0100

    cosmetic

--- env/dev/clj/user.clj
+++ env/dev/clj/user.clj
@@ -1,28 +1,29 @@
 (ns user
   "Userspace functions you can run by default in your local REPL."
   (:require
-    [relembra.config :refer [env]]
-    [clojure.spec.alpha :as s]
-    [expound.alpha :as expound]
-    [mount.core :as mount]
-    [relembra.core :refer [start-app]]))
+   [relembra.config :refer [env]]
+   [clojure.spec.alpha :as s]
+   [expound.alpha :as expound]
+   [mount.core :as mount]
+   [relembra.core :refer [start-app]]
+   [datomic.api :as d]))
 
 (alter-var-root #'s/*explain-out* (constantly expound/printer))
 
 (add-tap (bound-fn* clojure.pprint/pprint))
 
-(defn start 
+(defn start
   "Starts application.
   You'll usually want to run this on startup."
   []
   (mount/start-without #'relembra.core/repl-server))
 
-(defn stop 
+(defn stop
   "Stops application."
   []
   (mount/stop-except #'relembra.core/repl-server))
 
-(defn restart 
+(defn restart
   "Restarts application."
   []
   (stop)
@@ -41,3 +42,4 @@
   (relembra.db.core/install-norms conn norms)
 
   (d/q '[:find ?n :where [_ :user/name ?n]] (d/db conn))
+  )
